1cf4172a45533ddce96e77e8b3b2f3a40388ae8e
alexawik <alexandra.wikstrom@gmail.com>
2019-04-09 18:23:42 +0300

    kayttaja

--- otHarkkaSovellus/src/main/java/ot/otharkkasovellus/Kayttaja.java
+++ otHarkkaSovellus/src/main/java/ot/otharkkasovellus/Kayttaja.java
@@ -2,11 +2,11 @@ package ot.otharkkasovellus;
 
 
 public class Kayttaja extends Kirjautuja {
-    private String kayttaja;
     private int pistemaara;
     
     public Kayttaja(String kayttajatunnus, String salasana) {
         super(kayttajatunnus, salasana);
+        this.pistemaara = 0;
     }
     
     public void lisaaPisteita(int maara) {
@@ -14,7 +14,7 @@ public class Kayttaja extends Kirjautuja {
     }
     
     public int getPistemaara() {
-        return pistemaara;
+        return this.pistemaara;
     }
     
 }
